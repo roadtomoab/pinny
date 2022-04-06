fa8d48574e88aced339b771adaf2891b1f27d2ab
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-04-06 11:38:16 -0400

    fixed marker - now stays in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import Map from 'react-map-gl';
+import { useState } from 'react';
+import Map, { Marker } from 'react-map-gl';
+import { Room } from "@material-ui/icons";
 
 function App() {
+
+  const [viewState, setViewState] = useState({
+    longitude: -124,
+    latitude: 40,
+    zoom: 3.5
+  });
+
   return (
     <div>
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14
-        }}
-        style={{width: 600, height: 400}}
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+        style={{width: "100vw", height: "100vh"}}
+      >
+          <Marker
+          longitude={-124}
+          latitude={40}
+          anchor="bottom"
+          >
+            <Room style={{ 
+              width: 25,
+              height: 25,
+              color: "blue" }}/>
+          </Marker>
+      </Map>
     </div>
   );
 }
